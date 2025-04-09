2adc13a26cc5d8adb2a51537c3e61a1f6d34b4b7
zh-hadi <zhhadi50@gmail.com>
2025-04-09 09:56:56 +0600

    json-server add and api call read method

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react";
 import { useMyinfoContext } from "../context/MyinfoContext";
+
+
+
 
 const Home = () => {
 
     const {name} = useMyinfoContext();
+    const [post, setPost] = useState([]);
+
+
+    useEffect(()=> {
+
+        const fetchData = async () => {
+                try {
+                    const response = await fetch('http://localhost:3000/posts');
+        
+                    const data = await response.json();
+                    setPost(data)
+                    // console.log(data);
+                } catch (error) {
+                    console.log(error)
+                }
+        }
+
+        fetchData();
+    }, []);
 
   
+    console.log(post)
 
     return (<>
         <h2>Home Page</h2>
         <div>My name is {name} </div>
+
+        <div>
+            {post.map((item, index)=> (
+                <div key={index}>{item.title}</div>
+            ))}
+        </div>
     </>);
 }
 
